0bd9b48923e5f0e483c047f4be90a8e4bc72523a
DiceMast3r <babyisbap@gmail.com>
2024-07-11 00:22:33 +0700

    feat: Update number of bits in Exercise_2.py
    The code changes modify the number of bits in Exercise_2.py from 100 + 6 to 10. This change reduces the complexity of the code and aligns it with the desired behavior.

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.fftpack as fft_pack
 
-Nbits = 100 + 6          # No. bits
+Nbits = 10         # No. bits
 Nsamp = 8             # No. samples/bits
  
 #1. Generate data bits a, b #
 np.random.seed(30 + 6)
 a = np.random.randint(0 ,2 ,Nbits)
 b = 2 * a - 1
+
+print("a = ", a)
 
 # 2.Generate modulated signal m(t) #
 m = []
@@ -21,6 +23,9 @@ for i in range(Nbits):
 m_fft = fft_pack.fft(m)
 abs_m_fft = np.abs(m_fft)
 f = np.linspace(0, Nsamp, Nbits * Nsamp)
+
+print("Size of a = ", a.size)
+print("Size of m = ", len(m))
 
 # 3. Create a time axis
 t = np.arange(0, Nbits * Nsamp)
