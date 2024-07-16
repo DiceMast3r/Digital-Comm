45b8607ca6ce769aa11ab64679964657acf3d584
DiceMast3r <babyisbap@gmail.com>
2024-07-16 23:23:55 +0700

    Add Exercise_3

---
+++ Exercise_3.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import hist
+
+
+def compute_prob(x, a, b): #compute the probability of x in the range [a, b]
+    P = sum((x >= a) & (x <= b)) / x.size
+    return  P
+
+
+N = 10000 * 2
+n_normal = np.random.normal(0, 2, size=N)
+
+
+print("Variance = ", np.var(n_normal))
+print("Mean = ", np.mean(n_normal))
+
+P = sum((n_normal >= -1.5) & (n_normal <= 1.5)) / N # P(-1.5 <= n_normal <= 1.5)
+print("P(-1.5 <= n_normal <= 1.5) = ", compute_prob(n_normal, -1.5, 1.5))
+
+plt.figure(figsize=(10, 2))
+plt.plot(n_normal)
+plt.ylabel('n_normal')
+plt.xlabel('k')
+plt.title('Normal Distribution')
+
+plt.figure(figsize=(10, 2))
+hist(n_normal, density=True, bins=40)
+plt.ylabel('Probability')
+plt.xlabel('n_normal')
+
+
+plt.show()
